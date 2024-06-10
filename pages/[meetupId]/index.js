e9950e56473c7f11742f82c8e62536e9c31e9f01
Mariraj <maari000@hotmail.com>
2024-06-10 17:35:19 +0530

    first commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,67 @@
+import { MongoClient, ObjectId } from 'mongodb';
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+function MeetupDetails(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
+  );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect('mongodb+srv://maariraaj:Sharpener2024@cluster0.b76wgpk.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db();
+
+  const meetupsCollections = db.collection('meetups');
+
+  const meetups = await meetupsCollections.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: true,
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() }
+    }))
+  }
+};
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect('mongodb+srv://maariraaj:Sharpener2024@cluster0.b76wgpk.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db();
+
+  const meetupsCollections = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollections.findOne({ _id: new ObjectId(meetupId) });
+
+  client.close();
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        image: selectedMeetup.image,
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
+      }
+    },
+    revalidate: 1
+  };
+};
+
+export default MeetupDetails;
